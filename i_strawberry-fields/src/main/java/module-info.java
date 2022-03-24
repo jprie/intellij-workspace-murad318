342a0c5cfdfa6e30287a456a03a2a4e33eda4593
Johannes Priebsch <johannes@priebsch.at>
2022-03-24 16:41:33 +0100

    update i_strawberry-fields + FIX RunConfiguration

--- i_strawberry-fields/src/main/java/module-info.java
+++ i_strawberry-fields/src/main/java/module-info.java
@@ -1,6 +1,11 @@
 module at.muradundmurad.appdevs.coders318.i_strawberryfields {
     requires javafx.controls;
     requires javafx.fxml;
+
+      // FIXME: Problem ist, dass das Package keine Klassen mehr enthält
+        // FIXME: UND die RunConfiguration noch auf das alte package zeigt.
+    // Deswegen: RunConfiguration löschen und neu erstellen
+//    opens at.muradundmurad.appdevs.coders318.i_strawberryfields to javafx.fxml;
     exports at.muradundmurad.appdevs.coders318.i_strawberryfields.app;
     opens at.muradundmurad.appdevs.coders318.i_strawberryfields.app to javafx.fxml;
     exports at.muradundmurad.appdevs.coders318.i_strawberryfields.controller;
